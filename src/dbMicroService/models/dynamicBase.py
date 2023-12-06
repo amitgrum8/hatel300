e8c505cf09825ffb61dea61e22ed50ce6636aaf5
amitgrum8 <63203500+amitgrum8@users.noreply.github.com>
2023-12-06 11:23:37 +0200

    finished first api

--- src/dbMicroService/models/dynamicBase.py
+++ src/dbMicroService/models/dynamicBase.py
@@ -1,8 +1,2 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer
 
-Base = declarative_base()
 
-class DynamicBase(Base):
-    __abstract__ = True
-    id = Column(Integer, primary_key=True)
